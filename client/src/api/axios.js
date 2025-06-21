ddc9ecb08e8163140f749bd8aee18ea2a951a434
Shrishti Mishra <imshrishti0212@gmail.com>
2025-06-21 17:55:53 +0530

    last commit

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,23 +1,19 @@
-
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000',
-  withCredentials: true, 
-  headers:{
-    'Content-Type':'application/json',
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000',
+  withCredentials: true,
+  headers: {
+    'Content-Type': 'application/json',
   },
 });
 
-API.interceptors.request.use( (req) =>{
+API.interceptors.request.use((req) => {
   const token = localStorage.getItem('token');
-
-  if(token){
+  if (token) {
     req.headers.Authorization = `Bearer ${token}`;
   }
-
   return req;
-})
+});
 
 export default API;
-
